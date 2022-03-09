89e2e9a56ff4621c7c17c5398b502e41209a070a
Amen Olabode <olabodeamen4@gmail.com>
2022-03-09 10:40:49 +0000

    Start Python

---
+++ Decision.py
@@ -0,0 +1,8 @@
+age = 18
+
+if age < 18:
+    print("Still growing")
+elif age > 18 and age < 21:
+        print("You're a teenager")
+else:
+    print("You're an adult")
---
+++ first_prog.py
@@ -0,0 +1,17 @@
+Name = str('Amen')
+Age = 29
+EmployeeId = 12345
+FirsName = "Amen"
+LastName = "Olabode"
+BasicSalary = 2567895
+Currency = "NGN"
+allowance = 300
+PensionScheme = 0.02*BasicSalary
+tax = 0.01
+
+
+# Yen yen yen
+if Age == 29 and Name == "Amen":
+    print("Hello, I'm {name}, and I am {age}".format(name=Name, age=Age))
+else: 
+    print("Holy Holy Holy")
